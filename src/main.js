9916a0c348c8dd7ca07c991a3e68b41c4c50212c
Zheng ze Ou <ouzhengze@outlook.com>
2026-09-09 09:52:52 +0800

    Update image download filename to include date and time

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'sober';
 import { createScheme } from 'sober-theme';
 import { createRichTextEditor } from './richTextEditor';
 import changelogText from '../CHANGELOG.txt?raw';
+import { dayjs } from 'dayjs';
 
 // ==================== Dialog 工具（sober <s-dialog> 封装） ====================
 const Dialog = {
@@ -985,7 +986,7 @@ document.getElementById('save-btn').addEventListener('click', async () => {
         if (restorePanel) restorePanel.style.display = '';
         const imgData = canvas.toDataURL('image/png');
         const link = document.createElement('a');
-        link.download = 'MateriaHomework.png';
+        link.download = '作业_'+dayjs().format('YYYY-MM-DD_HHmmss')+'.png';
         link.href = imgData;
         link.click();
     } catch (err) {
